bf26eb92501693947f1d326a723f0ef89bfa7bf1
WeizenMehl <167409829+WeizenMehl@users.noreply.github.com>
2025-05-20 14:03:29 +0200

    Importet missing Components

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.first2dgame {
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    requires java.desktop;
 
     opens org.example.first2dgame to javafx.fxml;
     exports org.example.first2dgame;
--- src/main/java/org/example/first2dgame/GameFactory.java
+++ src/main/java/org/example/first2dgame/GameFactory.java
@@ -1,4 +1,77 @@
 package org.example.first2dgame;
 
+import com.almasb.fxgl.dsl.components.AutoRotationComponent;
+import com.almasb.fxgl.dsl.components.OffscreenCleanComponent;
+import com.almasb.fxgl.dsl.components.ProjectileComponent;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.entity.SpawnData;
+import com.almasb.fxgl.entity.Spawns;
+import com.almasb.fxgl.entity.components.IrremovableComponent;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import static com.almasb.fxgl.dsl.FXGLForKtKt.entityBuilder;
+import static com.almasb.fxgl.dsl.FXGLForKtKt.texture;
+
 public class GameFactory {
+
+    public enum EntityType {
+        BACKGROUND, CENTER, DUKE, CLOUD, BULLET
+    }
+
+    @Spawns("background")
+    public Entity spawnBackground(SpawnData data) {
+        return entityBuilder(data)
+                .type(EntityType.BACKGROUND)
+                .view(new Rectangle(data.<Integer>get("width"),
+                        data.<Integer>get("height"), Color.YELLOWGREEN))
+                .with(new IrremovableComponent())
+                .zIndex(-100)
+                .build();
+    }
+
+    @Spawns("center")
+    public Entity spawnCenter(SpawnData data) {
+        return entityBuilder(data)
+                .type(EntityType.CENTER)
+                .collidable()
+                .viewWithBBox(new Circle(data.<Integer>get("x"),
+                        data.<Integer>get("y"), data.<Integer>get("radius"), Color.DARKRED))
+                .with(new IrremovableComponent())
+                .zIndex(-99)
+                .build();
+    }
+
+    @Spawns("duke")
+    public Entity newDuke(SpawnData data) {
+        return entityBuilder(data)
+                .type(EntityType.DUKE)
+                .viewWithBBox(texture("duke.png", 50, 50))
+                .collidable()
+                .with((new AutoRotationComponent()).withSmoothing())
+                .with(new PlayerComponent())
+                .build();
+    }
+
+    @Spawns("cloud")
+    public Entity newCloud(SpawnData data) {
+        return entityBuilder(data)
+                .type(EntityType.CLOUD)
+                .viewWithBBox(texture("cloud-network.png", 50, 50))
+                .with((new AutoRotationComponent()).withSmoothing())
+                .with(new CloudComponent())
+                .collidable()
+                .build();
+    }
+
+    @Spawns("bullet")
+    public Entity newBullet(SpawnData data) {
+        return entityBuilder(data)
+                .type(EntityType.BULLET)
+                .viewWithBBox(texture("sprite_bullet.png", 22, 11))
+                .collidable()
+                .with(new ProjectileComponent(data.get("direction"), 350), new OffscreenCleanComponent())
+                .build();
+    }
 }
