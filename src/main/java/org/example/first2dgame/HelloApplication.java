a577003dbd836106a567b11448b6c930a1f6b4eb
WeizenMehl <167409829+WeizenMehl@users.noreply.github.com>
2025-05-20 14:38:51 +0200

    Edited main method

--- src/main/java/org/example/first2dgame/HelloApplication.java
+++ src/main/java/org/example/first2dgame/HelloApplication.java
@@ -1,23 +1,38 @@
 package org.example.first2dgame;
 
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.Entity;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
-public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+import static com.almasb.fxgl.app.GameApplication.launch;
+
+public class First2dGame extends GameApplication {
+    private final GameFactory gameFactory = new GameFactory();
+    private Entity player;
+
+    private static int screenWidth;
+    private static int screenHeight;
 
     public static void main(String[] args) {
-        launch();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        screenWidth = (int) screenSize.getWidth();
+        screenHeight = (int) screenSize.getHeight();
+        launch(args);
+    }
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        settings.setHeight(screenHeight);
+        settings.setWidth(screenWidth);
+        settings.setFullScreenAllowed(true);
+        settings.setFullScreenFromStart(true);
+        settings.setTitle("Oracle Java Magazine - FXGL");
     }
 }
